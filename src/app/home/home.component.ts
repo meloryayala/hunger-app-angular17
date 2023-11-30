e1fe7117dbd3085efa22fec7f9adb10731757d80
Melory Ayala <melory.ayala@gmail.com>
2023-11-30 19:47:17 +0100

    feat: add food to home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 import {FoodService} from "../services/food/food.service";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
